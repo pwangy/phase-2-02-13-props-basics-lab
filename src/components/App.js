44822319a77c52cf914e8ed6b0c0b7e864e102c8
pwangy <blipsandclicks@gmail.com>
2024-02-19 18:35:47 -0800

    clean up js files

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
-import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
+import NavBar from './NavBar'
+import Home from './Home'
+import About from './About'
 
 // pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+import user from '../data/user'
+console.log(user)
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<Home />
+			<About />
+		</div>
+	)
 }
 
-export default App;
+export default App
